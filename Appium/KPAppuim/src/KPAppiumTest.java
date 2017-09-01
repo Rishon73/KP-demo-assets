b86d301fa091b7b7450d05c60c69cc4590fbe20a
Rishon73 <sshiff@gmail.com>
2017-09-01 13:40:24 -0400

    Improved error handling and logging

--- Appium/KPAppuim/src/KPAppiumTest.java
+++ Appium/KPAppuim/src/KPAppiumTest.java
@@ -13,13 +13,15 @@ import io.appium.java_client.TouchAction;
 public class KPAppiumTest {
     private AndroidDriver driver = null;
     private final static String zipCode = "90210";
+    private enum LOG_LEVEL {INFO, ERROR};
+    private static boolean noProblem = true;
 
     @Before
     public void setup() {
         // Global vars for setup
         String MC_SERVER = "http://demo.mobilecenter.io:8080";
-        String MC_SERVER_USER = "";
-        String MC_SERVER_PASSWORD = "";
+        String MC_SERVER_USER = "shahar.shiff@hpe.com";
+        String MC_SERVER_PASSWORD = "Password1";
         String APP_PACKAGE = "org.kp.m";
         String APP_ACTIVITY = "org.kp.m.activity.SplashActivity";
 
@@ -29,6 +31,7 @@ public class KPAppiumTest {
 
             // Set device capabilities
             capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("platformVersion", "6.0.1");
 
             // Application capabilities
             capabilities.setCapability("appPackage", APP_PACKAGE);
@@ -37,99 +40,108 @@ public class KPAppiumTest {
             // Set MC Server credentials (could be skipped if "Anonymous access" is enabled for Appium scripts in the Administration settings).
             capabilities.setCapability("userName", MC_SERVER_USER);
             capabilities.setCapability("password", MC_SERVER_PASSWORD);
-            System.out.println("================== " + MC_SERVER + "/wd/hub ==================");
+            logMessages("================== " + MC_SERVER + "/wd/hub ==================", LOG_LEVEL.INFO);
 
             // Create a session to the MC server
             driver = new AndroidDriver(new URL(MC_SERVER + "/wd/hub"), capabilities);
-            System.out.println("MC session was successfully created [Android Device]");
+            logMessages("MC session was successfully created [Android Device]", LOG_LEVEL.INFO);
 
             // Create a wait object instance in order to verify expected conditions.
             WebDriverWait waitController = new WebDriverWait(driver, 60);
 
             // Create an implicitly wait instance to define the timeout for 'findElement' commands
-            driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+            driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-            System.out.println("=== Android WebDriver object created successfully ===");
+            logMessages("=== Android WebDriver object created successfully ===", LOG_LEVEL.INFO);
         } catch (MalformedURLException e) {
-            System.out.println("[ERROR]\n=== Malformed MC server URL ===\n" + e.getMessage() + "\nStack:\n" + e.getSuppressed());
-        } catch (Exception e) {
-            System.out.println("[ERROR]\n=== Test failed ===\n" + e.getMessage() + "\nStack:\n" + e.getSuppressed());
-            e.printStackTrace();
+            logMessages("=== Malformed MC server URL ===\n" + e.getMessage(), LOG_LEVEL.ERROR);
+            noProblem = false;
+        } catch (org.openqa.selenium.WebDriverException wde) {
+            logMessages("=== Test failed: WebDriverException ===\n" + wde.getMessage(), LOG_LEVEL.ERROR);
+            wde.printStackTrace();
+            noProblem = false;
         }
     }
 
     @Test
     public void TestFindFacility() {
-        WebElement element;
-        element = driver.findElementById("org.kp.m:id/sign_in_button");
-        element.click();
-
-        element = driver.findElementById("android:id/button1");
-        element.click();
-
-        System.out.println("Click Find facility button");
-        element = driver.findElementById("org.kp.m:id/sign_in_facility_locator");
-        element.click();
-
-        System.out.println("Check if 'Allow location...' is there...");
-        if (isElementPresent("com.android.packageinstaller:id/permission_allow_button")) {
-            element = driver.findElementById("com.android.packageinstaller:id/permission_allow_button");
+        if (!noProblem) return;
+        try {
+            logMessages("Device in use: " + driver.getCapabilities().getCapability("deviceName").toString() +
+                    ", version: " + driver.getCapabilities().getCapability("platformVersion").toString(), LOG_LEVEL.INFO);
+            WebElement element;
+            element = driver.findElementById("org.kp.m:id/sign_in_button");
             element.click();
-        }
 
-        System.out.println("Click the 'Close' important message");
-        element = driver.findElementById("android:id/button2");
-        element.click();
-
-        System.out.println("Click the search icon");
-        element = driver.findElementByAccessibilityId("SearchIcon");
-        element.click();
-
-        System.out.println("Type the zipcode");
-        element = driver.findElementById("org.kp.m:id/search_address_edit_text");
-        element.click();
-        element.sendKeys(zipCode);
-
-        System.out.println("Tap on the Google Maps 'search' icon");
-        TouchAction touchAction=new TouchAction(driver);
-        touchAction.press(1121, 1492).waitAction(200).release().perform();
-        //touchAction.tap(1150, 1280).perform();
-
-        windowSync(1500);
-
-        System.out.println("Open filters");
-        element = driver.findElementByAccessibilityId("Filter");
-        element.click();
-
-        System.out.println("Additional Services");
-        element = driver.findElementById("org.kp.m:id/checked_row_text_view");
-        element.click();
-
-        System.out.println("Click Ok");
-        element = driver.findElementById("android:id/button1");
-        element.click();
-
-        System.out.println("Display List view");
-        element = driver.findElementById("org.kp.m:id/locator_list_button");
-        element.click();
-
-        System.out.println("Select a facility");
-        element = driver.findElementById("org.kp.m:id/locator_favorites_row_title");
-        element.click();
-
-        System.out.println("Check if 'Allow access to contact...' is there...");
-        if (isElementPresent("com.android.packageinstaller:id/permission_allow_button")) {
-            element = driver.findElementById("com.android.packageinstaller:id/permission_allow_button");
+            element = driver.findElementById("android:id/button1");
             element.click();
+
+            logMessages("Click Find facility button", LOG_LEVEL.INFO);
+            element = driver.findElementById("org.kp.m:id/sign_in_facility_locator");
+            element.click();
+
+            logMessages("Check if 'Allow location...' is there...", LOG_LEVEL.INFO);
+            if (isElementPresent("com.android.packageinstaller:id/permission_allow_button")) {
+                element = driver.findElementById("com.android.packageinstaller:id/permission_allow_button");
+                element.click();
+            }
+
+            logMessages("Click the 'Close' important message", LOG_LEVEL.INFO);
+            element = driver.findElementById("android:id/button2");
+            element.click();
+
+            logMessages("Click the search icon", LOG_LEVEL.INFO);
+            element = driver.findElementByAccessibilityId("SearchIcon");
+            element.click();
+
+            logMessages("Type the zipcode", LOG_LEVEL.INFO);
+            element = driver.findElementById("org.kp.m:id/search_address_edit_text");
+            element.click();
+            element.sendKeys(zipCode);
+
+            logMessages("Tap on the Google Maps 'search' icon", LOG_LEVEL.INFO);
+            TouchAction touchAction=new TouchAction(driver);
+            touchAction.press(1121, 1492).waitAction(200).release().perform();
+            //touchAction.tap(1150, 1280).perform();
+
+            windowSync(1500);
+
+            logMessages("Open filters", LOG_LEVEL.INFO);
+            element = driver.findElementByAccessibilityId("Filter");
+            element.click();
+
+            logMessages("Additional Services", LOG_LEVEL.INFO);
+            element = driver.findElementById("org.kp.m:id/checked_row_text_view");
+            element.click();
+
+            logMessages("Click Ok", LOG_LEVEL.INFO);
+            element = driver.findElementById("android:id/button1");
+            element.click();
+
+            logMessages("Display List view", LOG_LEVEL.INFO);
+            element = driver.findElementById("org.kp.m:id/locator_list_button");
+            element.click();
+
+            logMessages("Select a facility", LOG_LEVEL.INFO);
+            element = driver.findElementById("org.kp.m:id/locator_favorites_row_title");
+            element.click();
+
+            logMessages("Check if 'Allow access to contact...' is there...", LOG_LEVEL.INFO);
+            if (isElementPresent("com.android.packageinstaller:id/permission_allow_button")) {
+                element = driver.findElementById("com.android.packageinstaller:id/permission_allow_button");
+                element.click();
+            }
+            logMessages("================== Test completed ==================", LOG_LEVEL.INFO);
+        } catch (Exception ex) {
+            logMessages("error in script. " + ex.getMessage(), LOG_LEVEL.ERROR);
         }
-        System.out.println("================== Test completed ==================");
     }
 
     @After
     public void tearDown() {
         // Release lock in all cases
         if (driver != null) {
-            System.out.println("MC session closed");
+            logMessages("MC session closed", LOG_LEVEL.INFO);
             driver.quit();
         }
     }
@@ -147,7 +159,16 @@ public class KPAppiumTest {
         try {
             Thread.sleep(milliseconds);
         } catch (InterruptedException err){
-            System.out.println("[Error] error in windowSync(int duration): " + err.getMessage());
+            logMessages("Error in windowSync(int duration): " + err.getMessage(), LOG_LEVEL.ERROR);
         }
+    }
+
+    private String getTimeStamp(String pattern) {
+        return new java.text.SimpleDateFormat(pattern).format(new java.util.Date());
+    }
+
+    private void logMessages(String message, LOG_LEVEL level) {
+        String prefix = (level==LOG_LEVEL.INFO) ? "[INFO] " : "[ERROR] ";
+        System.out.println(prefix + " [" + getTimeStamp("dd/MM/yyyy HH:mm:ss") + "] " + message);
     }
 }
