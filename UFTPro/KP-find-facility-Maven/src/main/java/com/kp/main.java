2066a1b320ed40ace83a709a860b8cd7229e6f02
Rishon73 <sshiff@gmail.com>
2017-09-07 20:47:03 -0400

    Removed main.java - don't need it anymore
    Not creating jar file

--- UFTPro/KP-find-facility-Maven/src/main/java/com/kp/main.java
+++
@@ -1,9 +0,0 @@
-package com.kp;
-
-import org.junit.runner.JUnitCore;
-public class main {
-    public static void main(String args[]){
-        JUnitCore.runClasses(LeanFtTest.class);
-        return;
-    }
-}
